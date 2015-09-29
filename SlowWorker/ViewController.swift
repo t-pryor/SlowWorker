7f510bc6a6511eb8036060eb1cd03781d196d8b7
Tim Pryor <Tim@Chantals-MacBook-Pro.local>
2015-09-29 09:52:50 -0400

    Initial Commit

--- SlowWorker/ViewController.swift
+++ SlowWorker/ViewController.swift
@@ -9,15 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var resultsTextView: UITextView!
+    
+    func fetchSomethingFromServer() -> String {
+        NSThread.sleepForTimeInterval(1)
+        return "Hi there"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func processData(data: String) -> String {
+        NSThread.sleepForTimeInterval(2)
+        return data.uppercaseString
+    }
+    
+    func calculateFirstResult(data: String) -> String {
+        NSThread.sleepForTimeInterval(3)
+        return "Number of chars: \(count(data))"
+    }
+    
+    func calculateSecondResult(data: String) -> String {
+        NSThread.sleepForTimeInterval(4)
+        return data.stringByReplacingOccurrencesOfString("E", withString: "e")
+    }
+    
+    @IBAction func doWork(sender: AnyObject) {
+        let startTime = NSDate()
+        self.resultsTextView.text = ""
+        let fetchedData = self.fetchSomethingFromServer()
+        let processedData = self.processData(fetchedData)
+        let firstResult = self.calculateFirstResult(processedData)
+        let secondResult = self.calculateSecondResult(processedData)
+        let resultsSummary = "First: [\(firstResult)]\nSecond: [\(secondResult)]"
+        self.resultsTextView.text = resultsSummary
+        let endTime = NSDate()
+        println("Completed in \(endTime.timeIntervalSinceDate(startTime)) second")
+        
     }
 
 
