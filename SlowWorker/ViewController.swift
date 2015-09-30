8db3496d29cf78f5d389bdde703a99db719f23f8
Tim Pryor <Tim@Chantals-MacBook-Pro.local>
2015-09-30 10:00:51 -0400

    Added GCD code to run closure on the background queue

--- SlowWorker/ViewController.swift
+++ SlowWorker/ViewController.swift
@@ -35,17 +35,48 @@ class ViewController: UIViewController {
     }
     
     @IBAction func doWork(sender: AnyObject) {
+        
         let startTime = NSDate()
         self.resultsTextView.text = ""
-        let fetchedData = self.fetchSomethingFromServer()
-        let processedData = self.processData(fetchedData)
-        let firstResult = self.calculateFirstResult(processedData)
-        let secondResult = self.calculateSecondResult(processedData)
-        let resultsSummary = "First: [\(firstResult)]\nSecond: [\(secondResult)]"
-        self.resultsTextView.text = resultsSummary
-        let endTime = NSDate()
-        println("Completed in \(endTime.timeIntervalSinceDate(startTime)) second")
         
+        // grab a preexisting global queue that's always avail
+        // two arg, 1st being a priority, secong unused and should always be 0
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        
+        // make this code run entirely in the background by wrapping all the 
+        // code in a closure and pass to a GCD function called dispatch_async
+        // Two parameters: a GCD queue and a closure to assign it to the queue
+        
+        // uses Swift's trailing closure syntax to make code more readable
+        /* normal syntax:
+        dispatch_queue_async(queue, {
+            //Code
+        })
+
+
+        */
+        
+        // GCD takes the closure and puts it on the queue, form where it will be scheduled to
+        // run on a background thread and executed one step at a time
+        
+        dispatch_async(queue) {
+            let fetchedData = self.fetchSomethingFromServer()
+            let processedData = self.processData(fetchedData)
+            let firstResult = self.calculateFirstResult(processedData)
+            let secondResult = self.calculateSecondResult(processedData)
+            let resultsSummary = "First: [\(firstResult)]\nSecond: [\(secondResult)]"
+           
+            // messaging any GUI object from a background thread is forbidden
+            // This passes work back to main thread
+            dispatch_async(dispatch_get_main_queue()) {
+                self.resultsTextView.text = resultsSummary
+            }
+            let endTime = NSDate()
+            // startTime defined before closure created
+            // by the time the closure is executed, doWork() has returned
+            // Swift automatically allows closure to access
+            println("Completed in \(endTime.timeIntervalSinceDate(startTime)) second")
+        }
     }
 
 
